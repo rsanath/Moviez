358e04c5b8c1380573d744cb971ef5f2d79a3d70
rsanath <ramsaanath@gmail.com>
2022-11-01 13:30:31 +0530

    use coroutines to make api call

--- app/src/main/java/com/example/moviez/movies/MoviesActivity.kt
+++ app/src/main/java/com/example/moviez/movies/MoviesActivity.kt
@@ -10,39 +10,23 @@ import com.example.moviez.R
 import com.example.moviez.data.MoviesApiService
 import com.example.moviez.data.MoviesResponse
 import com.example.moviez.models.Movie
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Response
+import java.io.IOException
+import kotlin.coroutines.CoroutineContext
 
-class MoviesActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity(), CoroutineScope {
     private val moviesApi by lazy { MoviesApiService.create() }
     private val adapter by lazy { MoviesAdapter() }
+    private val parentJob = SupervisorJob()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies)
         initUI()
-
-        moviesApi.getPopular(Constants.MOVIESDB_API_KEY)
-            .enqueue(object : retrofit2.Callback<MoviesResponse> {
-                override fun onResponse(
-                    call: Call<MoviesResponse>,
-                    response: Response<MoviesResponse>
-                ) {
-                    Toast.makeText(
-                        this@MoviesActivity,
-                        "isSuccessful = ${response.isSuccessful}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    val moviesResponse = response.body()
-                    print(moviesResponse)
-                    moviesResponse?.results?.let { populateMoviesList(it) }
-                }
-
-                override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
-                    t.printStackTrace()
-                }
-
-            })
+        launch { fetchData() }
     }
 
     private fun initUI() {
@@ -51,7 +35,31 @@ class MoviesActivity : AppCompatActivity() {
         list.layoutManager = LinearLayoutManager(this)
     }
 
-    private fun populateMoviesList(movies: List<Movie>) {
-        adapter.data = movies
+    private suspend fun fetchData() = withContext(Dispatchers.IO) {
+        try {
+            val response = moviesApi.getPopular(Constants.MOVIESDB_API_KEY).execute()
+            withContext(Dispatchers.Main) {
+                response.body()?.results?.let { adapter.data = it }
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(
+                    this@MoviesActivity,
+                    "Unable to get movies",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            e.printStackTrace()
+
+
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        parentJob.cancelChildren()
+    }
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + parentJob
 }
